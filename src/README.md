7af9e9113668f2fbe4533e040374e2998dc11179
Oscar Sandford <oscarsandford@gmail.com>
2022-02-11 16:10:11 -0800

    Add VM setup instr., sample code, editorconfig.

--- src/README.md
+++ src/README.md
@@ -1,3 +1,26 @@
 # /src
 
 Source code for our implementation will be stored here.
+
+## Environment
+
+To make the replication of our study easier, we use a Mininet VM (virtual machine) to demonstrate our QoS testing framework. 
+
+### Setup
+
+1. Install [VirtualBox](https://www.virtualbox.org/wiki/Downloads)) or another virtual machine program of your choice.
+2. Download the Mininet VM image for Ubuntu 20.04 [here](https://github.com/mininet/mininet/releases/download/2.3.0/mininet-2.3.0-210211-ubuntu-20.04.1-legacy-server-amd64-ovf.zip).
+3. Follow the Mininet setup instructions [here](http://mininet.org/vm-setup-notes/). If using VirtualBox, use the `.ovf` file unless it gives you trouble.
+4. Follow the instruction to log in to the VM on the same page.
+5. Create a directory `~/qoemf`. Place the source files here.
+
+If anyone can demonstrate how to either copy text or share files between this VM and the parent machine, please amend these setup instructions.
+
+### Tools
+
+On this VM: Vim, Python.
+
+## Mininet References
+
+* [Intro to Mininet](https://github.com/mininet/mininet/wiki/Introduction-to-Mininet) with some great sample code.
+* [API Reference](http://mininet.org/api/hierarchy.html)
---
+++ src/main.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+
+# Based on sample code for Mininet ping test.
+
+from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.node import CPULimitedHost
+from mininet.link import TCLink
+from mininet.util import dumpNodeConnections
+from mininet.log import setLogLevel
+from time import sleep
+
+class SingleSwitchTopo(Topo):
+	def build(self, n=2):
+		switch = self.addSwitch("s1")
+		for h in range(n):
+			# Each host gets 50%/n of system CPU
+			host = self.addHost( "h%s" % (h + 1), cpu=.5/n )
+			# 10 Mbps, 5ms delay, 2% loss, 1000 packet queue
+			self.addLink(host, switch, bw=10, delay="5ms", loss=2, max_queue_size=1000, use_htb=True)
+
+def startup_test(net):
+	print("Host connections")
+	dumpNodeConnections(net.hosts)
+	print("Test network connectivity with pingAll")
+	net.pingAll()
+
+def iperf_test(net, a: str, b: str):
+	print(f"Testing bandwidth between {a} and {b}.")
+	ha, hb = net.get(a, b)
+	net.iperf((ha, hb))
+
+def host_process_test(net, h: str):
+	print(f"Host {h} starting process")
+	host = net.get(h)
+	host.cmd("while true; do date; sleep 1; done > date.out &")
+	sleep(10)
+	print("Process complete")
+	host.cmd("kill %while")
+	print("Output:")
+	with open("dates.out", "r") as fl:
+		for i, line, in enumerate(fl.readlines()):
+			print(f"{i+1}: {line}")
+		fl.close()
+
+
+if __name__ == "__main__":
+	setLogLevel("info")
+	topo = SingleSwitchTopo(n=4)
+	net = Mininet(topo=topo, host=CPULimitedHost, link=TCLink)
+	net.start()
+
+	# Run tests
+	startup_test(net)
+	iperf_test(net, "h1", "h3")
+	host_process_test(net, "h1")
+
+	net.stop()
